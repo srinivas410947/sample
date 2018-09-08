d5e739407621ec2f65ad450cf19a289db0029f40
root <root@USSLTCSNP2745.solutions.glbsnet.com>
2018-09-08 07:47:50 -0400

    Auto-Commit

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /data/stage/git/sample
-git add git_push_testing/
+git add ./git_push_testing
 git add *
 git commit -m "Auto-Commit"
 git push https://github.com/srinivas410947/sample.git master
