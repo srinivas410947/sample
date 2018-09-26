34f2345a32838a9cf3c8e6a351372927ed28e061
srini <srinivas.kosana@lcbo.com>
2018-09-26 05:59:11 -0400

    Auto-Commit

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /appl/was/wcbd/git_testing/sample
-git pull https://github.com/srinivas410947/sample.git
-git checkout testing
+#cd /appl/was/wcbd/git_testing/sample
+#git pull https://github.com/srinivas410947/sample.git
+git fetch && git checkout testing
 cd /appl/was/wcbd/git_testing/sample
 git add *
 git commit -m "Auto-Commit"
